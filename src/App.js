c6f755d7a970a72b7516de56ab98a0d76576df1a
Jesschica <jessica.pean@neuf.fr>
2024-10-14 16:18:48 +0200

    creation pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import {BrowserRouter} from 'react-router-dom';
+import {Routes, Route} from"react-router-dom"
+
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Realisations from './pages/Realisations';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+
 function App() {
   return (
-    <div className="App">
+    <div>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/Realisations" element={<Realisations />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
       
     </div>
   );
